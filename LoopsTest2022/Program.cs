3a00dd270e43511e6f1d2864a29fb785e845cbe6
Poul Henriksen <pohe@easj.dk>
2022-10-04 12:54:21 +0200

    Add project files.

---
+++ LoopsTest2022/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Reflection.PortableExecutable;
+
+//int number = 1;
+//while (number < 5)
+//{
+//    Console.WriteLine($"The value of the number is {number}");
+//    number = number + 2;
+//}
+
+//udskriv alle tal fra 20 til og med 0 i faldende orden
+
+//int newNumber = 20;
+//while (newNumber>=0)
+//{
+//    Console.WriteLine($"The value of the newNumber is {newNumber}");
+//    newNumber = newNumber - 1;
+//}
+
+//udskriv alle lige tal fra 20 til og med 0 i faldende orden
+
+//int newNumber = 20;
+//while (newNumber >= 0)
+//{
+//    if (newNumber % 2 == 0)
+//    {
+//        Console.WriteLine($"The value of the newNumber is {newNumber}");
+//    }
+//    newNumber = newNumber - 1;
+//}
+
+
+//string keyPressed="";
+
+//while (keyPressed != "q")
+//{
+//    keyPressed = Console.ReadLine();
+//    Console.WriteLine($"You pressed {keyPressed}");
+//}
+
+                                   
+//for (int number = 20; number >=0 ; number--)
+//{
+//    if (number%2== 0)
+//        Console.WriteLine(number);
+//}
+
+//for(int number = 20; number >= 0; number= number -2)
+//{
+//    Console.WriteLine(number);
+//}
+
+//int number = 1; 
+//while (number < 5)
+//{
+//    Console.WriteLine(number);
+//    number++;  //number = number + 1
+//}
+
+
+for(int i=0; i<100; i++)
+{
+    if (i % 10 == 0 && i != 0)
+        Console.WriteLine();
+    Console.Write("*");
+    
+}
+
+
+//for(int i = 0; i<10; i++)
+//{
+//    for(int j = 0; j < 10; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+
+
